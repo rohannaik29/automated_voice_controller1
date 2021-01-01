6abb2eaa07d7763a6ef96557d1614b5070772a33
rohan <rrnaik2908@gmail.com>
2021-01-01 15:11:51 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:avatar_glow/avatar_glow.dart';
+import 'package:automatedvoicecontroller/streaming_page.dart';
 import 'package:flutter/material.dart';
-import 'package:highlight_text/highlight_text.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'speech_to_text_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,126 +13,33 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Voice',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SpeechScreen(),
-    );
-  }
-}
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.record_voice_over)),
+                  Tab(icon: Icon(Icons.video_label)),
 
-class SpeechScreen extends StatefulWidget {
-  @override
-  _SpeechScreenState createState() => _SpeechScreenState();
-}
-
-class _SpeechScreenState extends State<SpeechScreen> {
-  final Map<String, HighlightedWord> _highlights = {
-    'left': HighlightedWord(
-      onTap: () => print('left'),
-      textStyle: const TextStyle(
-        color: Colors.green,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'right': HighlightedWord(
-      onTap: () => print('right'),
-      textStyle: const TextStyle(
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'stop': HighlightedWord(
-      onTap: () => print('stop'),
-      textStyle: const TextStyle(
-        color: Colors.red,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'backward': HighlightedWord(
-      onTap: () => print('backward'),
-      textStyle: const TextStyle(
-        color: Colors.greenAccent,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'forward': HighlightedWord(
-      onTap: () => print('forward'),
-      textStyle: const TextStyle(
-        color: Colors.teal,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-  };
-
-  stt.SpeechToText _speech;
-  bool _isListening = false;
-  String _text = 'Press the button and start speaking';
-  double _confidence = 1.0;
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-        animate: _isListening,
-        glowColor: Theme.of(context).primaryColor,
-        endRadius: 75.0,
-        duration: const Duration(milliseconds: 2000),
-        repeatPauseDuration: const Duration(milliseconds: 100),
-        repeat: true,
-        child: FloatingActionButton(
-          onPressed: _listen,
-          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-        ),
-      ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-          child: TextHighlight(
-            text: _text,
-            words: _highlights,
-            textStyle: TextStyle(
-              fontSize: 32.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w400,
+                ],
+              ),
+              title: Text('Voice Controller'),
+              centerTitle: true,
             ),
-          ),
-        ),
+            body: TabBarView(
+              children: [
+                SpeechScreen(),
+                StreamingPage(),
+
+              ],
+            ),
+          )
       ),
     );
   }
-
-  void _listen() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
-        onError: (val) => print('onError: $val'),
-      );
-      if (available) {
-        setState(() => _isListening = true);
-        _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            if (val.hasConfidenceRating && val.confidence > 0) {
-              _confidence = val.confidence;
-            }
-          }),
-        );
-      }
-    } else {
-      setState(() => _isListening = false);
-      _speech.stop();
-    }
-  }
 }
+
---
+++ lib/streaming_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+Container btn(String dir,IconData ic ){
+  return Container(
+    width: 120.0,
+    child: RaisedButton(
+        onPressed: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(dir),
+            Icon(ic),
+          ],
+        )),
+  );
+}
+
+class StreamingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        //
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              btn('Forward', Icons.arrow_upward),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  btn('Left', Icons.arrow_back),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  btn('Stop', Icons.stop_circle_outlined),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  btn('Right', Icons.arrow_forward),
+                ],
+              ),
+              btn('Backward', Icons.arrow_downward),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
