060785231c8f7b900eef00615407f15323818ecb
Vrutik01 <vrutikparvadiya01@gmail.com>
2021-02-13 15:42:56 +0530

    3 V

--- lib/streaming_page.dart
+++ lib/streaming_page.dart
@@ -1,52 +1,138 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+import 'package:video_player/video_player.dart';
 
-Container btn(String dir,IconData ic ){
+Container btn(String dir, Size size) {
   return Container(
-    width: 120.0,
+    width: size.width * 0.29,
+    height: size.height * 0.06,
     child: RaisedButton(
-        onPressed: () {},
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(dir),
-            Icon(ic),
-          ],
-        )),
+      onPressed: () {},
+      child: Text(dir),
+    ),
   );
 }
 
-class StreamingPage extends StatelessWidget {
+class StreamingPage extends StatefulWidget {
+  @override
+  _StreamingPageState createState() => _StreamingPageState();
+}
+
+class _StreamingPageState extends State<StreamingPage> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Container(
-        //
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              btn('Forward', Icons.arrow_upward),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Center(
+                child: Column(
+                  children: [
+                    FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          // If the VideoPlayerController has finished initialization, use
+                          // the data it provides to limit the aspect ratio of the video.
+                          return AspectRatio(
+                            aspectRatio: _controller.value.aspectRatio,
+                            // Use the VideoPlayer widget to display the video.
+                            child: VideoPlayer(_controller),
+                          );
+                        } else {
+                          // If the VideoPlayerController is still initializing, show a
+                          // loading spinner.
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.2,
+              ),
+              Column(
                 children: [
-                  btn('Left', Icons.arrow_back),
+                  btn('Forward', size),
                   SizedBox(
-                    width: 10.0,
+                    height: 10.0,
                   ),
-                  btn('Stop', Icons.stop_circle_outlined),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      btn('Left', size),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      btn('Stop', size),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      btn('Right', size),
+                    ],
+                  ),
                   SizedBox(
-                    width: 10.0,
+                    height: 10.0,
                   ),
-                  btn('Right', Icons.arrow_forward),
+                  btn('Backward', size),
                 ],
               ),
-              btn('Backward', Icons.arrow_downward),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
