bb8ba0eb4f23038ac2565b834d65b53e40925772
rohan <rrnaik2908@gmail.com>
2021-02-13 14:28:35 +0530

    initial commit

--- lib/streaming_page.dart
+++ lib/streaming_page.dart
@@ -1,22 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-Container btn(String dir,IconData ic ){
-  return Container(
-    width: 120.0,
-    child: RaisedButton(
-        onPressed: () {},
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(dir),
-            Icon(ic),
-          ],
-        )),
+
+
+Flexible btn(String dir, IconData ic) {
+  return Flexible(
+    child: Container(
+      width: 120.0,
+      child: RaisedButton(
+          onPressed: () {},
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: Colors.deepPurple[100],
+              style: BorderStyle.solid,
+            ),
+            borderRadius: BorderRadius.circular(10),
+
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(dir),
+              Icon(ic),
+            ],
+          )),
+    ),
   );
 }
 
-class StreamingPage extends StatelessWidget {
+class StreamingPage extends StatefulWidget {
   @override
+  _StreamingPageState createState() => _StreamingPageState();
+}
+
+class _StreamingPageState extends State<StreamingPage> {
+  String _streamUrl='http://samples.mplayerhq.hu/MPEG-4/embedded_subs/1Video_2Audio_2SUBs_timed_text_streams_.mp4';
+  String state;
+  bool isPlaying;
+  VlcPlayerController _vlcViewController;
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _vlcViewController = new VlcPlayerController(onInit: () {
+    _vlcViewController.play();
+    });
+    _vlcViewController.addListener(() {
+      var dur=_vlcViewController.duration;
+      state = _vlcViewController.playingState.toString();
+    });
+    super.initState();
+  }
+
+  void _incrementCounter() {
+
+    setState(() {
+
+      // if (_streamUrl != null) {
+      //   _streamUrl = null;
+      // } else {
+      //   _streamUrl = 'assets/testvideo.mp4';
+        //http://samples.mplayerhq.hu/MPEG-4/embedded_subs/1Video_2Audio_2SUBs_timed_text_streams_.mp4';
+
+        // 'http://192.168.1.4:8081'
+      _streamUrl='http://samples.mplayerhq.hu/MPEG-4/embedded_subs/1Video_2Audio_2SUBs_timed_text_streams_.mp4';
+
+    // }
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -24,8 +77,47 @@ class StreamingPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+              _streamUrl == null
+                  ? Expanded(
+                    child: Container(
+                      color: Colors.deepPurple,
+                        child: Center(
+                          child: RichText(
+                            text: TextSpan(children: [
+                              TextSpan(
+                                text: 'Stream Closed',
+                                style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    background: Paint()..color = Colors.red
+                                ),
+                              )
+                            ]),
+                          ),
+                        ),
+                      ),
+                  )
+                  : new VlcPlayer(
+                      url: _streamUrl,
+                      controller: _vlcViewController,
+                      aspectRatio: 3,
+                      placeholder: Container(),
+                    ),
+
+              FloatingActionButton(
+                onPressed: playOrPauseVideo,
+                tooltip: 'Increment',
+                child:
+                    Icon(state == 'PlayingState.PLAYING' ? Icons.play_arrow : Icons.pause),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+
               btn('Forward', Icons.arrow_upward),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -48,4 +140,21 @@ class StreamingPage extends StatelessWidget {
       ),
     );
   }
+  void playOrPauseVideo() {
+    String state = _vlcViewController.playingState.toString();
+
+
+    if (state == "PlayingState.PLAYING") {
+      _vlcViewController.pause();
+      setState(() {
+        isPlaying = false;
+      });
+    } else {
+      _vlcViewController.play();
+      setState(() {
+        isPlaying = true;
+      });
+    }
+  }
 }
+
