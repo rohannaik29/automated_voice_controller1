30342438b3e298501030d64db3b768680593894d
Vrutik01 <vrutikparvadiya01@gmail.com>
2021-03-01 16:25:29 +0530

    5 V

--- lib/streaming_page.dart
+++ lib/streaming_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'SendToRaspberryPi.dart';
 
 class StreamingPage extends StatefulWidget {
@@ -8,8 +8,6 @@ class StreamingPage extends StatefulWidget {
 }
 
 class _StreamingPageState extends State<StreamingPage> {
-  VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
 
   Container btn(String dir, Size size) {
     return Container(
@@ -22,27 +20,22 @@ class _StreamingPageState extends State<StreamingPage> {
     );
   }
 
+  String _streamUrl='http://192.168.43.1:8080/video';
+  String state;
+  bool isPlaying;
+  VlcPlayerController _vlcViewController;
   @override
   void initState() {
-    _controller = VideoPlayerController.network(
-      'http://192.168.43.1:8080/video1',
-    );
+    // TODO: implement initState
 
-    // Initialize the controller and store the Future for later use.
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    // Use the controller to loop the video.
-    _controller.setLooping(true);
-
+    _vlcViewController = new VlcPlayerController(onInit: () {
+      _vlcViewController.play();
+    });
+    _vlcViewController.addListener(() {
+      var dur=_vlcViewController.duration;
+      state = _vlcViewController.playingState.toString();
+    });
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
-    _controller.dispose();
-
-    super.dispose();
   }
 
   @override
@@ -58,23 +51,14 @@ class _StreamingPageState extends State<StreamingPage> {
               Center(
                 child: Column(
                   children: [
-                    FutureBuilder(
-                      future: _initializeVideoPlayerFuture,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          // If the VideoPlayerController has finished initialization, use
-                          // the data it provides to limit the aspect ratio of the video.
-                          return AspectRatio(
-                            aspectRatio: _controller.value.aspectRatio,
-                            // Use the VideoPlayer widget to display the video.
-                            child: VideoPlayer(_controller),
-                          );
-                        } else {
-                          // If the VideoPlayerController is still initializing, show a
-                          // loading spinner.
-                          return Center(child: CircularProgressIndicator());
-                        }
-                      },
+                    Container(
+                      height: 250.0,
+                      child: new VlcPlayer(
+                        url: _streamUrl,
+                        controller: _vlcViewController,
+                        aspectRatio: 3,
+                        placeholder: Container(),
+                      ),
                     ),
                   ],
                 ),
@@ -82,55 +66,37 @@ class _StreamingPageState extends State<StreamingPage> {
               SizedBox(
                 height: size.height * 0.2,
               ),
-              // Column(
-              //   children: [
-              //     btn('Forward', size),
-              //     SizedBox(
-              //       height: 10.0,
-              //     ),
-              //     Row(
-              //       mainAxisAlignment: MainAxisAlignment.center,
-              //       children: [
-              //         btn('Left', size),
-              //         SizedBox(
-              //           width: 10.0,
-              //         ),
-              //         btn('Stop', size),
-              //         SizedBox(
-              //           width: 10.0,
-              //         ),
-              //         btn('Right', size),
-              //       ],
-              //     ),
-              //     SizedBox(
-              //       height: 10.0,
-              //     ),
-              //     btn('Backward', size),
-              //   ],
-              // ),
+              Column(
+                children: [
+                  btn('Forward', size),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      btn('Left', size),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      btn('Stop', size),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      btn('Right', size),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  btn('Backward', size),
+                ],
+              ),
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Wrap the play or pause in a call to `setState`. This ensures the
-          // correct icon is shown.
-          setState(() {
-            // If the video is playing, pause it.
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              // If the video is paused, play it.
-              _controller.play();
-            }
-          });
-        },
-        // Display the correct icon depending on the state of the player.
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
