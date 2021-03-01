a441358dbe9e69af802715456522e459eebcee68
Vrutik01 <vrutikparvadiya01@gmail.com>
2021-03-01 17:58:30 +0530

    rasp ip

--- lib/SendToRaspberryPi.dart
+++ lib/SendToRaspberryPi.dart
@@ -7,7 +7,7 @@ class SendToRaspberryPi{
   void sendToRaspberryPi(String dir) {
     var client = http.Client();
     try {
-      var url = 'http://192.168.0.1:3000/$dir';
+      var url = 'http://169.254.74.142:3000/$dir';
       client.post(url, body: json.encode({'status': dir}), headers: {
         'Content-type': 'application/json'
       }).then((response) => print('status: $dir'));
